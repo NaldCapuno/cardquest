88ee2a69e49120b985fd689ab26347fa5d6262ac
NaldCapuno <jasoncapuno@gmail.com>
2023-12-01 20:05:17 +0800

    modified

--- projectsite/cardquest/urls.py
+++ projectsite/cardquest/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('trainer_list', views.TrainerList.as_view(), name='trainer-list'),
+    path('trainer', views.TrainerList.as_view(), name='trainer-list'),
     path('pokemon_card', views.PokemonCardList.as_view(), name='pokemon-card'),
     path('collection', views.CollectionList.as_view(), name='collection'),
 ]
